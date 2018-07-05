a9139fc41ef258096acdd9430ea868aad12368ed
Mary Francis <maryfsc@disroot.org>
2018-07-05 17:01:42 -0300

    Exercício resolvido em sala.

--- programadoras/assets/script.js
+++ programadoras/assets/script.js
@@ -1,3 +1,5 @@
+var dropMenu = document.getElementById('drop-menu');
+dropMenu.addEventListener('change', carregaProgramadoras);
 
 
 
@@ -6,5 +8,14 @@
 // };
 
 function carregaProgramadoras(){
-
+	var sede = dropMenu.value;
+	var listaProgramadoras = document.getElementById('exibe-programadoras');
+	listaProgramadoras.innerHTML = '';
+	for (turma in data[sede]) {
+		for (i in data[sede][turma]['students']) {
+			var img = document.createElement('img');
+			img.src = data[sede][turma]['students'][i]['photo'];
+			listaProgramadoras.appendChild(img);
+		}
+	}
 };
