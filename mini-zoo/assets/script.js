ef77cc62eb4fce9712bde683c3acf01e180b7b18
Mary Francis <maryfsc@disroot.org>
2018-07-05 16:21:27 -0300

    Exercício resolvido em sala.

--- mini-zoo/assets/script.js
+++ mini-zoo/assets/script.js
@@ -1,1 +1,25 @@
+var dropMenu = document.getElementById("drop-menu");
+dropMenu.addEventListener('change', clickEffect);
+var takePic = document.querySelectorAll('.thumbs-item'); /*pega todos elementos de classe e retorna um array*/
 
+function clickEffect() {
+  takePic.forEach(pic => {
+    var effect = dropMenu.value;
+    if (effect === 'sepia') {
+      pic.classList = 'thumbs-item';
+      pic.classList.add('sepia');
+    }
+    if (effect === 'grayscale') {
+      pic.classList = 'thumbs-item';
+      pic.classList.add('grayscale');
+    }
+    if (effect === 'invert') {
+      pic.classList = 'thumbs-item';
+      pic.classList.add('invert');
+    }
+    if (effect === '') {
+      pic.classList = 'thumbs-item';
+    }
+  });
+
+}
